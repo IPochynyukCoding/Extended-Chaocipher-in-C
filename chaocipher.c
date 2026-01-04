5448c269d66b22173ad28030e83fe9831f8e1559
IPochynyukCoding <ivanpochynyuk@gmail.com>
2026-01-03 22:41:18 -0600

    Added another prompt for more reusability.

--- chaocipher.c
+++ chaocipher.c
@@ -23,6 +23,12 @@ void strslice(char* original_string, char* slice,size_t start,size_t stop){
     slice[stop-start]='\0';
 }
 
+void menu_selection(struct choices* array, int size){
+    for(int i=0;i<size;i++){
+        printf("%d. %s\n",i+1,array[i].label);
+    }
+}
+
 char* case_insensitive(char* input, bool is_upper){
     char* copy=calloc(strlen(input)+1,sizeof(char));
     for(int i=0;i<strlen(input);i++){
@@ -30,6 +36,7 @@ char* case_insensitive(char* input, bool is_upper){
     }
     return copy;
 }
+
 
 char* ciphertext_shift(char* ciphertext_wheel,int index){
     size_t length = strlen(ciphertext_wheel);
@@ -136,17 +143,19 @@ int main(){
     char* plaintext_wheel;
     char* cipher_wheel;
     char* encoded;
-    char buffer[16];
-    char buffer_2[16];
-    char buffer_3[16];
+    char file_confirmation[16];
+    char selection_confirmation[16];
+    char character_set_confirmation[16];
+    char case_confirmation[16];
     char filename[128];
     char selection_label[16];
     FILE* input_file;
     FILE* output_file;
     time_t current_time;
     struct tm* local_time;
-    bool is_finished;
-    bool is_case_insensitive;
+    bool input_confirmation=false;
+    bool is_finished=false;
+    bool is_case_insensitive=false;
     struct choices choice_array[4] = {
         {"Uppercase letters","ABCDEFGHIJKLMNOPQRSTUVWXYZ",false},
         {"Lowercase letters","abcdefghijklmnopqrstuvwxyz",false},
@@ -161,52 +170,69 @@ int main(){
         exit(EXIT_FAILURE);
     }
     srand(time(NULL));
+    do{
     input_file = fopen("to_encode.txt","r");
     if(input_file==NULL ||!fgets(input_buffer,16384,input_file)){
         printf("Unable to read the file, exiting...");
         exit(EXIT_FAILURE);
     }
     fclose(input_file);
+    printf("Your input is %s\nDo you want to use this text to encode?\nPress 'y' for yes, 'q' for quit, or anything else to give you time to edit your input: ",input_buffer);
+    if(!fgets(file_confirmation,16,stdin)){
+        printf("Unable to get a reading, exiting...\n");
+        exit(EXIT_FAILURE);
+            }
+    if(strlen(file_confirmation)==2 && strnicmp(file_confirmation,"y",1)==0){
+            input_confirmation=true;
+        }
+    else if(strlen(file_confirmation)==2 && strnicmp(file_confirmation,"q",1)==0){
+        printf("Thank you for running the extended chaocipher program, exiting...\n");
+        exit(EXIT_SUCCESS);
+    }
+    }while (!input_confirmation);
+    selected_character_set[first_index]='\0';
     do{
         if(strlen(selected_character_set)>0){
             printf("%s\n",selected_character_set);
             printf("Press 'enter' to finish selection\n");
         }
+        menu_selection(choice_array,array_len);
         printf("Enter a number between 1 and 4 or press 'q' to quit: ");
-        if(!fgets(buffer,16,stdin)){
+        if(!fgets(selection_confirmation,16,stdin)){
             printf("Unable to get a reading, exiting...\n");
             exit(EXIT_FAILURE);
         }
         char *endptr;
         errno = 0;
-        long selection=strtol(buffer,&endptr,10);
-        if(strlen(buffer)==2 && strnicmp(buffer,"q",1)==0){
+        long selection=strtol(selection_confirmation,&endptr,10);
+        if(strlen(selection_confirmation)==2 && strnicmp(selection_confirmation,"q",1)==0){
             printf("Thank you for running the extended chaocipher program, exiting...\n");
             exit(EXIT_SUCCESS);
         }
-        else if(endptr==buffer && strlen(selected_character_set)>0 && *endptr == '\n'){
+        else if(endptr==selection_confirmation && strlen(selected_character_set)>0 && *endptr == '\n'){
             
             printf("Are you sure you are finished?\nYour current character set is: %s\nPress 'y' to continue with your character set for the cipher, 'q' to quit, or anything else to get out of this prompt: ",selected_character_set);
-            if(!fgets(buffer_2,16,stdin)){
+            if(!fgets(character_set_confirmation,16,stdin)){
                 printf("Unable to get a reading, exiting...\n");
                 exit(EXIT_FAILURE);
             }
-            if(strlen(buffer_2)==2 && strnicmp(buffer_2,"y",1)==0){
+            if(strlen(character_set_confirmation)==2 && strnicmp(character_set_confirmation,"y",1)==0){
                 if(is_uppercase+is_lowercase<2){
                 strcpy(selection_label,is_uppercase? "uppercase":"lowercase");
                 printf("Do you want your input to be case-insensitive (i.e, treat all letters as %s)\nPress 'y' to change all letters to %s, 'n' for no, 'q' to quit, or anything else to get out of this prompt: ",selection_label,selection_label);
-                if(!fgets(buffer_3,16,stdin)){
+                if(!fgets(case_confirmation,16,stdin)){
                 printf("Unable to get a reading, exiting...\n");
                 exit(EXIT_FAILURE);
                 }
-                if(strlen(buffer_3)==2 && strnicmp(buffer_3,"y",1)==0){
+                if(strlen(case_confirmation)==2 && strnicmp(case_confirmation,"y",1)==0){
                     case_insensitive_buffer=case_insensitive(input_buffer,is_uppercase);
+                    is_case_insensitive=true;
                     is_finished=true;
                 }
-                else if(strlen(buffer_3)==2 && strnicmp(buffer_3,"n",1)==0){
+                else if(strlen(case_confirmation)==2 && strnicmp(case_confirmation,"n",1)==0){
                     is_finished=true;
                 }
-                else if(strlen(buffer_3)==2 && strnicmp(buffer_3,"q",1)==0){
+                else if(strlen(case_confirmation)==2 && strnicmp(case_confirmation,"q",1)==0){
                 printf("Thank you for running the extended chaocipher program, exiting...\n");
                 exit(EXIT_SUCCESS);
                 }
@@ -215,7 +241,7 @@ int main(){
                  }
                 }
             }
-            else if(strlen(buffer_2)==2 && strnicmp(buffer_2,"q",1)==0){
+            else if(strlen(character_set_confirmation)==2 && strnicmp(character_set_confirmation,"q",1)==0){
                 printf("Thank you for running the extended chaocipher program, exiting...\n");
                 exit(EXIT_SUCCESS);
             }
@@ -232,12 +258,15 @@ int main(){
             printf("Added %s into the character set\n",choice_array[selection-1].label);
             printf("Current set:%s\n",selected_character_set);
         }
+        else if(choice_array[selection-1].is_selected){
+            printf("The option %s is already been selected!\n",choice_array[selection-1].label);
+        }
     }while(!is_finished);
     plaintext_wheel=wheel_generate(selected_character_set);
     cipher_wheel=wheel_generate(selected_character_set);
     wheel_uniqueness(plaintext_wheel,cipher_wheel);
     snprintf(original_wheels,512,"Ciphertext Wheel: %s\nPlaintext Wheel: %s",cipher_wheel,plaintext_wheel);
-    encoded=chaocipher_function(plaintext_wheel,cipher_wheel,input_buffer);
+    encoded= is_case_insensitive ? chaocipher_function(plaintext_wheel,cipher_wheel,case_insensitive_buffer):chaocipher_function(plaintext_wheel,cipher_wheel,input_buffer);
     snprintf(encoded_buffer,32768,"%s\nEncoded text: %s\n",original_wheels,encoded);
     current_time= time(NULL);
     local_time= localtime(&current_time);
